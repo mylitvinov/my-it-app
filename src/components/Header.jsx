346c4ad087f2f51862eec10a6e91fa68002380a0
mylitvinov <vinchester30@mail.ru>
2021-08-15 23:29:48 +0300

    css modules

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={s.header}>
+      <img src="https://st2.depositphotos.com/4035913/6124/i/600/depositphotos_61244231-stock-photo-3d-logo-sphere.jpg" />
+    </header>
+  );
+};
+
+export default Header;
