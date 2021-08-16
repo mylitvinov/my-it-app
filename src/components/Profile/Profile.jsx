f398f606fce41667cf28581b84deeb551880b452
mylitvinov <vinchester30@mail.ru>
2021-08-16 23:00:31 +0300

    edit project - props

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import s from './Profile.module.css'
+import MyPosts from "./MyPosts/MyPosts";
+import s from "./Profile.module.css";
 const Profile = () => {
   return (
     <div className={s.content}>
@@ -7,14 +8,7 @@ const Profile = () => {
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtyeBLv7LvP-kPXelQOpE1EQCcxZtSDw37d3QfEynRWysRvkD6RiMkS20SqpRU8shoDcO1vUQPTNkN5A&usqp=CAU" />
       </div>
       <div>ava + desription</div>
-      <div>
-        My posts
-        <div>New post</div>
-        <div className = {s.posts}>
-          <div className = {s.item}>Post 1</div>
-          <div className = {s.item}>Post 2</div>
-        </div>
-      </div>
+      <MyPosts />
     </div>
   );
 };
