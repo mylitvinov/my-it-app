749b22769365359d590258fb75ef824f719a755a
mylitvinov <vinchester30@mail.ru>
2021-08-29 16:26:47 +0300

    ref, onClick, Virtual DOM

--- src/App.js
+++ src/App.js
@@ -7,34 +7,32 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 const App = (props) => {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        <div className={"app-wrapper-content"}>
-          {/* <Route path = '/dialogs' component={Dialogs} />
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <div className={"app-wrapper-content"}>
+        {/* <Route path = '/dialogs' component={Dialogs} />
           <Route path = '/profile' component={Profile} /> 
            <Route path = '/news' component={News} />
           <Route path = '/music' component={Music} />
           <Route path = '/settings' component={Settings} />  */}
-          <Route
-            path="/dialogs"
-            render={() => <Dialogs state={props.state.dialogsPage} />}
-          />
-          <Route
-            path="/profile"
-            render={() => <Profile state={props.state.profilePage} />}
-          />
-          <Route path="/news" render={() => <News />} />
-          <Route path="/music" render={() => <Music />} />
-          <Route path="/settings" render={() => <Settings />} />
-        </div>
+        <Route
+          path="/dialogs"
+          render={() => <Dialogs state={props.state.dialogsPage} />}
+        />
+        <Route
+          path="/profile"
+          render={() => <Profile state={props.state.profilePage} />}
+        />
+        <Route path="/news" render={() => <News />} />
+        <Route path="/music" render={() => <Music />} />
+        <Route path="/settings" render={() => <Settings />} />
       </div>
-    </BrowserRouter>
+    </div>
   );
 };
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,10 +3,16 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 const MyPosts = (props) => {
 
-
   let postsElements = props.posts.map((p) => (
     <Post message={p.message} likesCount={p.likesCount} />
   ));
+
+  let newPostElement = React.createRef();
+
+  let addPost = ()=> {
+    let text = newPostElement.current.value
+     alert(text)
+  }
 
   return (
     <div className={s.postsBlock}>
@@ -14,10 +20,10 @@ const MyPosts = (props) => {
       <div>
         <div>
           {" "}
-          <textarea></textarea>
+          <textarea ref = {newPostElement}></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick = {addPost}>Add post</button>
         </div>
       </div>
       <div className={s.posts}>{postsElements}</div>
